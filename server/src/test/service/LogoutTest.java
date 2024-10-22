58ec129f47a24a4e2b44ad0a4292b38e1d6de61a
Blake <blakeedwincalkins@gmail.com>
2024-10-22 12:02:54 -0600

    init tests for successful and unsuccessful logouts

---
+++ server/src/test/service/LogoutTest.java
@@ -0,0 +1,31 @@
+package service;
+
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class LogoutTest {
+    private static LogoutService logoutService;
+    private static RegisterService registerService;
+
+    @BeforeAll
+    public static void setUp() {
+        registerService = new RegisterService(new RegisterRequest("Blake", "password", "blake@fake.com"));
+    }
+
+    @Test
+    public void testLogoutSuccessful() throws DataAccessException {
+        String authToken = registerService.registerUser().authToken();
+        logoutService = new LogoutService(new LogoutRequest(authToken));
+        LogoutResult result = logoutService.logoutUser();
+        Assertions.assertEquals("", result.errMessage());
+    }
+
+    @Test
+    public void testLogoutFail() throws DataAccessException {
+        logoutService = new LogoutService(new LogoutRequest(ServiceUtils.generateToken()));
+        LogoutResult result = logoutService.logoutUser();
+        Assertions.assertEquals("Not authorized", result.errMessage());
+    }
+}
