fceb32c412151ead977708ef2854ac4fcc545699
Blake <blakeedwincalkins@gmail.com>
2024-10-21 23:52:43 -0600

    implement pass and fail unit tests for RegisterService

---
+++ server/src/test/service/RegisterTest.java
@@ -0,0 +1,32 @@
+package service;
+
+import dataaccess.DataAccessException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RegisterTest {
+    private static RegisterService service;
+
+    @BeforeAll
+    public static void setUp() {
+        service = new RegisterService(new RegisterRequest("John", "pass", "john@yahoo.com"));
+    }
+
+    @Test
+    public void testAuthTokens() throws DataAccessException {
+        RegisterResult testResult = new RegisterResult("John", ServiceUtils.generateToken(), "");
+        RegisterResult actualResult = service.registerUser();
+        System.out.println("test result auth token:" + testResult.authToken());
+        System.out.print("actual result auth token:" + actualResult.authToken());
+        Assertions.assertNotEquals(testResult, actualResult);
+    }
+
+    @Test
+    public void testUserAlreadyExists() throws DataAccessException {
+        service.registerUser();
+        RegisterService testService = new RegisterService(new RegisterRequest("John", "pswd", "john@failure.com"));
+        String errMessage = testService.registerUser().message();
+        Assertions.assertEquals("Error: User already exists.", errMessage);
+    }
+}
