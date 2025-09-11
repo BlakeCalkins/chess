d23db8ef71237eb07c3539dece1d560c419d95bc
Blake <blakeedwincalkins@gmail.com>
2025-09-11 15:48:48 -0600

    add RookMovesCalculator with call to pieceMoves

---
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -0,0 +1,15 @@
+package chess;
+
+import java.util.Collection;
+
+public class RookMovesCalculator extends PieceMovesCalculator {
+
+    public RookMovesCalculator(ChessBoard board, ChessPosition position) {
+        super(board, position);
+    }
+
+    @Override
+    public Collection<ChessMove> pieceMoves() {
+        return super.straightMoves();
+    }
+}
