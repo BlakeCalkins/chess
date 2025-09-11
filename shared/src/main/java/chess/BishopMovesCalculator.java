be957ba460e05a0b34144beaf5555ba8c00a3241
Blake <blakeedwincalkins@gmail.com>
2025-09-11 15:50:50 -0600

    init BishopMovesCalculator constructor

--- shared/src/main/java/chess/BishopMovesCalculator.java
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -1,5 +1,10 @@
 package chess;
 
-public class BishopMovesCalculator {
+public class BishopMovesCalculator extends PieceMovesCalculator {
+
+    public BishopMovesCalculator(ChessBoard board, ChessPosition position) {
+        super(board, position);
+    }
+
 
 }
