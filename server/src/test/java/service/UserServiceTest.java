e3fbe4d10e955c0964aa744e6a8374b4a1226180
Blake <blakeedwincalkins@gmail.com>
2025-10-16 10:45:44 -0600

    init UserServiceTest

---
+++ server/src/test/java/service/UserServiceTest.java
@@ -0,0 +1,35 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.MemoryDataAccess;
+import datamodel.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void register() throws Exception {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData("joe", "j@j.com", "pswd");
+        var userService = new UserService(db);
+        var authData = userService.register(user);
+        assertNotNull(authData);
+        assertEquals(user.username(), authData.username());
+        assertFalse(authData.authToken().isEmpty());
+    }
+
+    // not correct yet
+    @Test
+    void registerInvalidUsername() throws Exception {
+        DataAccess db = new MemoryDataAccess();
+        var user = new UserData(null, "j@j.com", "pswd");
+        var userService = new UserService(db);
+        var authData = userService.register(user);
+        // change these
+        assertNotNull(authData);
+        assertEquals(user.username(), authData.username());
+        assertFalse(authData.authToken().isEmpty());
+    }
+}
