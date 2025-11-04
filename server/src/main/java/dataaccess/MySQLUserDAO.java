67a9f73fcdd6ee309edb2d89bfa9637a3b673351
Blake <blakeedwincalkins@gmail.com>
2025-11-04 16:21:35 -0700

    init MySQLUserDAO

---
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -0,0 +1,59 @@
+package dataaccess;
+
+import datamodel.UserData;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+public class MySQLUserDAO implements UserDataAccess {
+    @Override
+    public HashMap<String, UserData> getUsers() {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void createUser(UserData user) {
+
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public Boolean validPassword(UserData user) {
+        return null;
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  users (
+              `username` varchar(256) NOT NULL,
+              `password` varchar(256) NOT NULL,
+              `email` varchar(256) NOT NULL,
+              PRIMARY KEY (`username`),
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+    // not sure if works.
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
