18b829597da56a566741a92dc664c2fc68750710
Blake <blakeedwincalkins@gmail.com>
2025-11-05 10:04:47 -0700

    init MySQL User DAO tests

--- server/src/test/java/dataaccess/UserDataAccessTest.java
+++ server/src/test/java/dataaccess/UserDataAccessTest.java
@@ -1,31 +1,65 @@
 package dataaccess;
 
 import datamodel.UserData;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDataAccessTest {
 
+    UserData user = new UserData("blake", "blake@yahoo.com", "password");
+
     // petshop has example of test for phase 3 and 4 at same time.
+
+    @AfterEach
+    void clearOut() throws DataAccessException {
+        UserDataAccess db = new MySQLUserDAO();
+        db.clear();
+    }
 
     @Test
     void clear() throws DataAccessException {
-        UserDataAccess db = new MemoryUserDataAccess();
-        db.createUser(new UserData("joe", "j@j.com", "pswd"));
+        UserDataAccess db = new MySQLUserDAO();
+        db.createUser(user);
         db.clear();
         assertNull(db.getUser("joe"));
     }
 
     @Test
-    void createUser() throws DataAccessException {
-        UserDataAccess db = new MemoryUserDataAccess();
-        var user = new UserData("joe", "j@j.com", "pswd");
+    void createUserAndGetUser() throws DataAccessException {
+        UserDataAccess db = new MySQLUserDAO();
         db.createUser(user);
-        assertEquals(user, db.getUser(user.username()));
+        assertEquals(user.username(), db.getUser(user.username()).username());
+        assertEquals(user.email(), db.getUser(user.username()).email());
+        assertNotEquals(user.password(), db.getUser(user.username()).password());
     }
 
     @Test
-    void getUser() {
+    void createUserTaken() throws DataAccessException {
+        UserDataAccess db = new MySQLUserDAO();
+        db.createUser(user);
+        assertThrows(RuntimeException.class,() -> db.createUser(user));
+    }
+
+    @Test
+    void getUserBadUser() throws DataAccessException {
+        UserDataAccess db = new MySQLUserDAO();
+        db.createUser(user);
+        assertNull(db.getUser(new UserData("jorge", "jorge@hotmail.com", "pass").username()));
+    }
+
+    @Test
+    void validPassword() throws DataAccessException {
+        UserDataAccess db = new MySQLUserDAO();
+        db.createUser(user);
+        assertTrue(db.validPassword(user));
+    }
+
+    @Test
+    void invalidPassword() throws DataAccessException {
+        UserDataAccess db = new MySQLUserDAO();
+        db.createUser(user);
+        assertFalse(db.validPassword(new UserData("blake", "blake@yahoo.com", "bad_password")));
     }
 }
