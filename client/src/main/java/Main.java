741a287332f8d5b659eede6a2556e2cd9f8cd199
Blake <blakeedwincalkins@gmail.com>
2025-11-13 15:28:31 -0700

    init post-login repl with minimal functionality

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,11 +1,7 @@
-import chess.*;
-
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("♕ Welcome to the 240 Chess Client. Type help to get started.");
-        label:
+    private static boolean preLoginLoop() {
         while (true) {
             System.out.print("[LOGGED_OUT] >>> ");
             Scanner scanner = new Scanner(System.in);
@@ -19,6 +15,7 @@ public class Main {
                         System.out.printf("Too %s inputs, type in the format: register <USERNAME> <PASSWORD> <EMAIL>%n", amt);
                     } else {
                         System.out.println("registering...");
+                        // ep.register(username commands[1], password commands[2], email commands[3])
                     }
                     break;
                 case "login":
@@ -27,15 +24,104 @@ public class Main {
                         System.out.printf("Too %s inputs, type in the format: login <USERNAME> <PASSWORD> %n", amt);
                     } else {
                         System.out.println("logging in...");
+                        // ep.login(username commands[1], password commands[2])
+                        return true;
                     }
                     break;
                 case "quit":
-                    break label;
+                    System.out.println("Bye bye!");
+                    return false;
                 default:
                     System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
                     System.out.println("login <USERNAME> <PASSWORD> - to play chess");
                     System.out.println("quit - playing chess");
                     System.out.println("help - with possible commands");
+            }
+        }
+    }
+
+    private static boolean postLoginLoop() {
+        while (true) {
+            System.out.print("[LOGGED_IN] >>> ");
+            Scanner scanner = new Scanner(System.in);
+            String line = scanner.nextLine();
+            var commands = line.split(" ");
+            String command = commands[0];
+            switch (command) {
+                case "create":
+                    if (commands.length != 2) {
+                        String amt = (commands.length > 2) ? "many" : "few";
+                        System.out.printf("Too %s inputs, type in the format: create <NAME> %n", amt);
+                    } else {
+                        System.out.printf("creating game %s...", commands[1]);
+                        System.out.println();
+                        // ep.create(name, auth)
+                    }
+                    break;
+                case "list":
+                    System.out.println("Current games: ");
+                    // ep.listgames(auth)
+                    continue;
+                case "join":
+                    if (commands.length != 3) {
+                        String amt = (commands.length > 3) ? "many" : "few";
+                        System.out.printf("Too %s inputs, type in the format: join <ID> [WHITE/BLACK] %n", amt);
+                    } else if (isNotInteger(commands[1])) {
+                        System.out.println("Please provide an integer for the game ID.");
+                    } else {
+                        System.out.printf("joining game %d...", Integer.parseInt(commands[1]));
+                        System.out.println();
+                        // ep.join(id, auth)
+                    }
+                    break;
+                case "observe":
+                    if (commands.length != 2) {
+                        String amt = (commands.length > 3) ? "many" : "few";
+                        System.out.printf("Too %s inputs, type in the format: observe <ID> %n", amt);
+                    } else if (isNotInteger(commands[1])) {
+                        System.out.println("Please provide an integer for the game ID.");
+                    } else {
+                        System.out.printf("observing game %d...", Integer.parseInt(commands[1]));
+                        System.out.println();
+                        // ep.printStoredGame(id)
+                    }
+                    break;
+                case "logout":
+                    System.out.println("logging you out...");
+                    return true;
+                    // ep.logout(auth)
+                case "quit":
+                    System.out.println("Bye bye!");
+                    // ep.logout(auth)
+                    return false;
+                default:
+                    System.out.println("create <NAME> - a game");
+                    System.out.println("list - current games");
+                    System.out.println("join <ID> [WHITE/BLACK] - a game");
+                    System.out.println("observe <ID> - a game");
+                    System.out.println("logout - when you are done");
+                    System.out.println("quit - playing chess");
+                    System.out.println("help - with possible commands");
+            }
+        }
+    }
+
+    public static boolean isNotInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return false;
+        } catch (NumberFormatException e) {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("♕ Welcome to the 240 Chess Client. Type help to get started.");
+        while (true) {
+            if (preLoginLoop()) {
+                if (!postLoginLoop()) {
+                    break;
+                }
             }
         }
     }
